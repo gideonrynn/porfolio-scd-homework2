694199c162e005060cb1c7f8c983c72cf1c073fc
gideonrynn <sarah.c.dillard@gmail.com>
2020-02-24 20:21:15 -0600

    added portfolio js with stored project objects and script js for rendering cards

---
+++ assets/js/projects.js
@@ -0,0 +1,135 @@
+//Projects stored as an array of objects. New projects can be added here for display on page by adding another object
+const projects = [
+    {
+      category: "features",
+      title: "What's To Do",
+      screenshot: "assets/images/plan.jpg",
+      image_alt: "Day Planner Image",
+      app_link: "https://gideonrynn.github.io/whats-to-do-day-planner-homework5/",
+      github_link: "https://github.com/gideonrynn/whats-to-do-day-planner-homework5",
+      stretched_link: "https://gideonrynn.github.io/whats-to-do-day-planner-homework5/"
+    },
+    {
+        category: "features",
+        title: "Fulcrum Employee CMS",
+        screenshot: "assets/images/fulcrumapp.JPG",
+        image_alt: "Fulcrum App Image",
+        app_link: "",
+        github_link: "https://github.com/gideonrynn/fulcrum-employee-tracker-cms-homework12",
+        stretched_link: "https://github.com/gideonrynn/fulcrum-employee-tracker-cms-homework12"
+        
+      },
+    
+      {
+        category: "features",
+        title: "Feed Harvey Logger",
+        screenshot: "assets/images/harvey.jpg",
+        image_alt: "Harvey Image",
+        app_link: "https://fathomless-lake-45236.herokuapp.com/",
+        github_link: "https://github.com/gideonrynn/feed-harvey-food-logger-hw13",
+        stretched_link: "https://fathomless-lake-45236.herokuapp.com/"
+        
+      },
+      
+      {
+        category: "features",
+        title: "Best Day Out Tracker",
+        screenshot: "assets/images/bestdayout.jpg",
+        image_alt: "Project Image",
+        app_link: "https://andrelbarreto.github.io/ProjectOne/",
+        github_link: "https://github.com/gideonrynn/ProjectOne",
+        stretched_link: "https://andrelbarreto.github.io/ProjectOne/"
+        
+      },
+
+      {
+        category: "features",
+        title: "Life+ Self-Care App",
+        screenshot: "assets/images/lifeplustracker.JPG",
+        image_alt: "Health Boi Image",
+        app_link: "https://life-plus-app.herokuapp.com/",
+        github_link: "https://github.com/gideonrynn/life-plus-project2",
+        stretched_link: "https://life-plus-app.herokuapp.com/"
+        
+      },
+
+      {
+        category: "other",
+        title: "Fifth Element Quiz",
+        screenshot: "assets/images/fifthelement.jpg",
+        image_alt: "Quiz Screenshot Image",
+        app_link: "https://gideonrynn.github.io/fifthelement-code-quiz-homework4/",
+        github_link: "https://github.com/gideonrynn/fifthelement-code-quiz-homework4",
+        stretched_link: "https://gideonrynn.github.io/fifthelement-code-quiz-homework4/"
+        
+      },
+
+      {
+        category: "other",
+        title: "Password Generator",
+        screenshot: "assets/images/password.jpg",
+        image_alt: "Password Generator Image",
+        app_link: "https://gideonrynn.github.io/password-generator-homework3/",
+        github_link: "https://github.com/gideonrynn/password-generator-homework3",
+        stretched_link: "https://gideonrynn.github.io/password-generator-homework3/"
+        
+      },
+
+      {
+        category: "other",
+        title: "Weather Wizard Dashboard",
+        screenshot: "assets/images/weather.jpg",
+        image_alt: "Weather Wizard Image",
+        app_link: "https://gideonrynn.github.io/weather-wizard-dashboard-homework6/",
+        github_link: "https://github.com/gideonrynn/weather-wizard-dashboard-homework6",
+        stretched_link: "https://gideonrynn.github.io/weather-wizard-dashboard-homework6/"
+        
+      },
+
+      {
+        category: "other",
+        title: "TeamPacR Profile Generator",
+        screenshot: "assets/images/teampacrimg.JPG",
+        image_alt: "TeamPacR Image",
+        app_link: "",
+        github_link: "https://github.com/gideonrynn/teampacr-profile-generator-hw7",
+        stretched_link: "https://github.com/gideonrynn/teampacr-profile-generator-hw7"
+        
+      },
+
+      {
+        category: "other",
+        title: "Team Sprite Site Generator",
+        screenshot: "assets/images/teamsprite.JPG",
+        image_alt: "TeamSprite Image",
+        app_link: "",
+        github_link: "https://github.com/gideonrynn/teamsprite-site-generator-hw10",
+        stretched_link: "https://github.com/gideonrynn/teamsprite-site-generator-hw10"
+        
+      },
+
+      {
+        category: "other",
+        title: "Duly Noted Note Taker",
+        screenshot: "assets/images/dulynoted.JPG",
+        image_alt: "TeamSprite Image",
+        app_link: "https://hidden-retreat-96452.herokuapp.com/",
+        github_link: "https://github.com/gideonrynn/dulynoted-note-taker-hw11",
+        stretched_link: "https://hidden-retreat-96452.herokuapp.com/"
+        
+      },
+
+      {
+        category: "comingsoon",
+        title: "",
+        screenshot: "assets/images/comingsoon.jpg",
+        image_alt: "Coming Soon Image",
+        app_link: "",
+        github_link: "",
+        stretched_link: ""
+        
+      }
+
+
+  ];
+  
---
+++ assets/js/script.js
@@ -0,0 +1,73 @@
+$(function () {
+
+    //define rows to which cards will be appended
+    const projectFeatures = $('.projectfeatures');
+    const projectOther = $('.projectsother');
+    const projectSoon = $('.comingsoon');
+
+    // let title = ;
+
+    function renderCards () {
+
+        //define for loop for iterating through projects object and adding div and image elements to page
+        for (let i = 0; i < projects.length; i++) {
+
+            //create new column element and add class
+            let newColumn = $("<div>").addClass("col-lg-4 col-md-6 col-sm-12 my-1");
+
+            //create new card element and add class
+            let newCardDiv = $("<div>").addClass("card card-hov bg-white text-white mw-100 mh-100");
+
+            let newImg = $("<img>").addClass("card-img").attr("src", projects[i].screenshot).attr("alt", projects[i].image_alt);
+
+            let newImgOverlay = $("<div>").addClass("card-img-overlay d-flex justify-content-center align-items-end");
+
+            let newCardText = $("<p>").addClass("card-text p-2").text(projects[i].title).append("<br/>");
+
+            let newCardLink = $("<a>").addClass("portfolio-link").attr("href", projects[i].app_link).text("App");
+
+            let newRepoLink = $("<a>").addClass("portfolio-link").attr("href", projects[i].github_link).text("Repo");
+
+            let newSeparator = $("<a>").attr("href", "#").attr("target", "_blank").text("  " + "|" + "  ");
+
+            let newStrLink = $("<a>").addClass("stretched-link").attr("target", "_blank").attr("href", projects[i].stretched_link);
+
+            let targetRow = "";
+
+            //set targetRow based on category of project
+            if (projects[i].category == "features") {
+
+                targetRow = projectFeatures;
+
+            } else if (projects[i].category == "other") {
+
+                targetRow = projectOther;
+
+            } else {
+                targetRow = projectSoon;
+            }
+
+            //create new column
+            targetRow.append(newColumn);
+
+            //create new card div
+            newColumn.append(newCardDiv);
+
+            //add image and image overlay
+            newCardDiv.append(newImg, newImgOverlay);
+
+            //add contents of card div
+            newImgOverlay.append(newCardText, newStrLink);
+
+            //add links
+            newCardText.append(newCardLink, newSeparator, newRepoLink);
+
+        };
+
+    }
+
+
+    //when the portfolio page opens, render cards from data in projects object
+    renderCards();
+
+});
